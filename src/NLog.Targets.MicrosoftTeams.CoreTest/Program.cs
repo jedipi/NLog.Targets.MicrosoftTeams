163cf90f42b57bdb767eb23faf305a03ee8337b1
jedipi <forjedi@gmail.com>
2021-02-05 20:58:26 +1100

    Add .Net Core test project

---
+++ src/NLog.Targets.MicrosoftTeams.CoreTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NLog.Targets.MicrosoftTeams.CoreTest
+{
+    class Program
+    {
+        private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            //_logger.Info("Info.");
+            _logger.Fatal(new ArgumentException("42"), "Fatal!");
+
+            Console.ReadKey();
+        }
+    }
+}
